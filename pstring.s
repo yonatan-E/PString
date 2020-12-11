246f8e4924bfc9e82f4cfb3fe48ec463e43ff810
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-12-11 22:44:12 +0200

    Minor changes

--- pstring.s
+++ pstring.s
@@ -1,126 +1,125 @@
-            .section        .rodata
-invalid_string:     .string         "invalid input!\n"
+        .section        .rodata
+invalid_input_str:     .string     "invalid input!\n"
             
-            .text
-
+        .text
 # this function calculates the length of a given string
-            .type	pstrlen, @function
+        .type	pstrlen, @function
 pstrlen:
-            movb    (%rdi), %al
-            ret
+        movsbq  (%rdi), %rax
+        ret
 
 # this function replaces all of the instances of a given char in a given string with another char
-            .type replaceChar, @function
+        .type   replaceChar, @function
 replaceChar:
-            call    pstrlen
-            xorb    %cl, %cl
+        call    pstrlen
+        xorb    %cl, %cl
 loop:
-            xorb    $1(%rdi, %al, ), %sil
-            jne     not_equals
-            movb    %dl, $1(%rdi, %al, )
+        xorb    $1(%rdi, %al, ), %sil
+        jne     not_equals
+        movb    %dl, 1(%rdi, %al, )
 not_equals:
-            incb    %cl
-            cmpb    %cl, %al
-            jg      loop
+        incb    %cl
+        cmpb    %cl, %al
+        jg      loop
 
-            movq    %rdi, %rax
-            ret
+        movq    %rdi, %rax
+        ret
 
 # this function copies a substring of a given string to another given string
-            .type pstrijcpy, @function
+        .type   pstrijcpy, @function
 pstrijcpy:
-            call    pstrlen
-            cmpb    %al, %dl
-            jg      index_out_of_range
-            cmpb    %al,  %cl
-            jg      index_out_of_range
+        call    pstrlen
+        cmpb    %al, %dl
+        jg      index_out_of_range
+        cmpb    %al,  %cl
+        jg      index_out_of_range
 
-            pushq   %rdi
-            movq    %rsi, %rdi
-            call    pstrlen
-            popq    %rdi
-            cmpb    %al, %dl
-            jg      index_out_of_range
-            cmpb    %al,  %cl
-            jg      index_out_of_range
+        pushq   %rdi
+        movq    %rsi, %rdi
+        call    pstrlen
+        popq    %rdi
+        cmpb    %al, %dl
+        jg      index_out_of_range
+        cmpb    %al,  %cl
+        jg      index_out_of_range
 
-            cmpb    %cl, %dl
-            jg      done
+        cmpb    %cl, %dl
+        jg      done
 loop:
-            movb    $1(%rsi, %dl, ), $1(%rdi, %dl, )
-            incb    %dl
-            cmpb    %cl, %dl
-            jle     loop
+        movb    1(%rsi, %dl, ), 1(%rdi, %dl, )
+        incb    %dl
+        cmpb    %cl, %dl
+        jle     loop
 done:
-            movq    %rdi, %rax
-            ret
+        movq    %rdi, %rax
+        ret
 index_out_of_range:
-            movq    $invalid_string, %rdi
-            movq    $0, %rax
-            call    printf
-            jmp     done
+        movq    $invalid_input_str, %rdi
+        movq    $0, %rax
+        call    printf
+        jmp     done
 
 # this function replaces all of the upper case letters in a given string to normal case 
-            .type swapCase, @function
+        .type   swapCase, @function
 swapCase:
-            call    pstrlen
-            xorb    %sil, %sil
+        call    pstrlen
+        xorb    %sil, %sil
 loop:
-            movb    $1(%rdi, %sil, ), %dl
-            cmpb    %dl, $65
-            jg      not_upper_case
-            cmpb    $90, %dl
-            jg      not_upper_case
-            addb    $32, %dl
-            movb    %dl, $1(%rdi, %sil, )
+        movb    1(%rdi, %sil, ), %dl
+        cmpb    %dl, $65
+        jg      not_upper_case
+        cmpb    $90, %dl
+        jg      not_upper_case
+        addb    $32, %dl
+        movb    %dl, $1(%rdi, %sil, )
 not_upper_case:
-            incb    %sil
-            cmpb    %sil, %al
-            jg      loop
+        incb    %sil
+        cmpb    %sil, %al
+        jg      loop
 
-            movq    %rdi, %rax
-            ret
+        movq    %rdi, %rax
+        ret
 
 # this function compares lexicographily between substrings of two given strings 
-            .type pstrijcmp, @function
+        .type   pstrijcmp, @function
 pstrijcmp:
-            call    pstrlen
-            cmpb    %al, %dl
-            jg      index_out_of_range
-            cmpb    %al,  %cl
-            jg      index_out_of_range
+        call    pstrlen
+        cmpb    %al, %dl
+        jg      index_out_of_range
+        cmpb    %al,  %cl
+        jg      index_out_of_range
 
-            pushq   %rdi
-            movq    %rsi, %rdi
-            call    pstrlen
-            popq    %rdi
-            cmpb    %al, %dl
-            jg      index_out_of_range
-            cmpb    %al,  %cl
-            jg      index_out_of_range
+        pushq   %rdi
+        movq    %rsi, %rdi
+        call    pstrlen
+        popq    %rdi
+        cmpb    %al, %dl
+        jg      index_out_of_range
+        cmpb    %al,  %cl
+        jg      index_out_of_range
 
-            cmpb    %cl, %dl
-            jg      done
+        cmpb    %cl, %dl
+        jg      done
 loop:
-            cmpb    $1(%rsi, %dl, ), $1(%rdi, %dl, )
-            jg      s1_is_greater
-            jl      s2_is_greater
+        cmpb    1(%rsi, %dl, ), 1(%rdi, %dl, )
+        jg      s1_is_greater
+        jl      s2_is_greater
 
-            incb    %dl
-            cmpb    %cl, %dl
-            jle     loop
+        incb    %dl
+        cmpb    %cl, %dl
+        jle     loop
 done:
-            movq    $0, %rax
-            ret
+        movq    $0, %rax
+        ret
 s1_is_greater:
-                movq    $1, %rax
-                ret
+        movq    $1, %rax
+        ret
 s1_is_greater:
-                movq    $-1, %rax
-                ret
+        movq    $-1, %rax
+        ret
 index_out_of_range:
-            movq    $invalid_string, %rdi
-            movq    $0, %rax
-            call    printf
-            movq    $-2, %rax
-            ret
+        movq    $invalid_input_str, %rdi
+        movq    $0, %rax
+        call    printf
+        movq    $-2, %rax
+        ret
